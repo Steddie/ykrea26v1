7a3094d6b7c2a76e9d5608f8ee2174fef2374e2a
Johan Nordström <johan@digitalvillage.se>
2026-02-24 19:53:17 +0100

    Modern JS - `var` vs `let/const`.

--- 01-modern-js-and-ts/src/modern-js.js
+++ 01-modern-js-and-ts/src/modern-js.js
@@ -1,3 +1,44 @@
 import "./style.css";
 
-console.log("It works!");
+/**
+ * 🥴
+ */
+var count = 1;
+
+if (true) {
+	var count = 2;
+	console.log("(var) Inside if-statement:", count);  // 2
+}
+
+console.log("(var) Outside if-statement:", count);  // 2 😱
+
+
+console.log("(var) x before declaration:", x);  // undefined - no error! 😱😱
+var x = 10;
+console.log("(var) x after declaration:", x);  // 10
+
+function varTest() {
+	var x = 5;
+	console.log("(var) Inside varTest-function:", x);
+}
+varTest();
+console.log("(var) After varTest-function:", x);
+
+/**
+ * 🤩
+ */
+
+let total = 5;
+// let total = 42;  // nope! 🚔
+
+if (true) {
+	let total = 10;
+	console.log("(let) Inside if-statement:", total);
+}
+console.log("(let) Outside if-statement:", total);
+
+// console.log("(let) y before declaration:", y);  // Uncaught ReferenceError: can't access lexical declaration 'y' before initialization
+// let y = 42;
+
+// const msg = "Hello world!";
+// msg = "Good night!";  // Uncaught TypeError: invalid assignment to const 'msg'
