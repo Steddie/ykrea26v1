75196520f962617a5bf1308b12c13d2a4b2f399f
Johan Nordström <johan@digitalvillage.se>
2026-03-03 20:45:15 +0100

    Show/hide salary section. Also, Fragments are cool.

--- 02-react-basics/src/App.tsx
+++ 02-react-basics/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
 		{ id: 3, title: "Got state? 🚓", likes: 3 },
 	]);
 	const [salary, setSalary] = useState(10);
+	const [showSalary, setShowSalary] = useState(false);
 
 	const handleBtnClick = () => {
 		console.log("Counter before updates:", counter);  // 0
@@ -57,49 +58,60 @@ function App() {
 
 			<hr />
 
-			<h2>Salary</h2>
+			<button
+				className="btn btn-primary mb-4"
+				onClick={() => setShowSalary(!showSalary)}
+			>
+				Show/hide salary
+			</button>
 
-			<p>Salary per hour: {salary} &euro;</p>
+			{showSalary && (
+				<>
+					<h2>Salary</h2>
 
-			{salary < 10 && (
-				<div className="alert alert-warning">
-					You might want to change job?
-				</div>
+					<p>Salary per hour: {salary} &euro;</p>
+
+					{salary < 10 && (
+						<div className="alert alert-warning">
+							You might want to change job?
+						</div>
+					)}
+
+					<div className="buttons">
+						<div className="mb-1">
+							<button
+								className="btn btn-primary btn-lg"
+								onClick={() => handleChangeSalary(1)}
+							>
+								Raise 1 &euro; 🤑
+							</button>
+							<button
+								className="btn btn-warning btn-lg"
+								disabled={salary === 5}
+								onClick={() => handleChangeSalary(-1)}
+							>
+								Decrease 1 &euro; 😢
+							</button>
+						</div>
+
+						<div className="mb-1">
+							<button
+								className="btn btn-primary btn-lg"
+								onClick={() => handleChangeSalary(5)}
+							>
+								Raise 5 &euro; 🤑🤑🤑
+							</button>
+							<button
+								className="btn btn-warning btn-lg"
+								disabled={salary === 5}
+								onClick={() => handleChangeSalary(-5)}
+							>
+								Decrease 5 &euro; 😢😢😢
+							</button>
+						</div>
+					</div>
+				</>
 			)}
-
-			<div className="buttons">
-				<div className="mb-1">
-					<button
-						className="btn btn-primary btn-lg"
-						onClick={() => handleChangeSalary(1)}
-					>
-						Raise 1 &euro; 🤑
-					</button>
-					<button
-						className="btn btn-warning btn-lg"
-						disabled={salary === 5}
-						onClick={() => handleChangeSalary(-1)}
-					>
-						Decrease 1 &euro; 😢
-					</button>
-				</div>
-
-				<div className="mb-1">
-					<button
-						className="btn btn-primary btn-lg"
-						onClick={() => handleChangeSalary(5)}
-					>
-						Raise 5 &euro; 🤑🤑🤑
-					</button>
-					<button
-						className="btn btn-warning btn-lg"
-						disabled={salary === 5}
-						onClick={() => handleChangeSalary(-5)}
-					>
-						Decrease 5 &euro; 😢😢😢
-					</button>
-				</div>
-			</div>
 
 			<hr />
 
