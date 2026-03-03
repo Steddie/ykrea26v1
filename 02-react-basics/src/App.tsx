b692162b77d56fa2a94ccd1964aed745e13386c7
Johan Nordström <johan@digitalvillage.se>
2026-03-03 18:57:44 +0100

    Our first stateful variable.

--- 02-react-basics/src/App.tsx
+++ 02-react-basics/src/App.tsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-	let counter = 0;
+	// let counter = 0;  // stateless
+	const [counter, setCounter] = useState(0);
 
 	const handleBtnClick = () => {
 		console.log("Stop it, that tickles!");
 
 		console.log("Counter before increase:", counter);
 		// increase counter
-		counter++;
-		console.log("Counter after increase:", counter);
+		setCounter(counter + 1);
+		console.log("Counter after increase:", counter);  // still the same value since state updates are queued
 	}
+
+	console.log("App is rendering, counter is:", counter);
 
 	return (
 		<>
