a14becbeba25289a0b86e401715d85e5dbce61fe
Johan Nordström <johan@digitalvillage.se>
2026-03-03 20:38:31 +0100

    Use previous state when updating state.

--- 02-react-basics/src/App.tsx
+++ 02-react-basics/src/App.tsx
@@ -19,12 +19,15 @@ function App() {
 	const [salary, setSalary] = useState(10);
 
 	const handleBtnClick = () => {
-		console.log("Stop it, that tickles!");
+		console.log("Counter before updates:", counter);  // 0
 
-		console.log("Counter before increase:", counter);
-		// increase counter
-		setCounter(counter + 1);
-		console.log("Counter after increase:", counter);  // still the same value since state updates are queued
+		setCounter( (prevValue) => prevValue + 1 );  // prevValue = 0, return 1
+
+		console.log("Counter between updates:", counter);  // 0
+
+		setCounter( (prevValue) => prevValue + 1 );  // prevValue = 1, return 2
+
+		console.log("Counter after updates:", counter);  // 0
 	}
 
 	const handleChangeSalary = (amount: number) => {
