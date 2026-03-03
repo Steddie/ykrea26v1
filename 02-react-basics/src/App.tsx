cfd6667e48834fb454270847512d81b3b2d301e7
Johan Nordström <johan@digitalvillage.se>
2026-03-03 20:26:00 +0100

    Conditionally render a message if salary is below €10.

--- 02-react-basics/src/App.tsx
+++ 02-react-basics/src/App.tsx
@@ -58,6 +58,12 @@ function App() {
 
 			<p>Salary per hour: {salary} &euro;</p>
 
+			{salary < 10 && (
+				<div className="alert alert-warning">
+					You might want to change job?
+				</div>
+			)}
+
 			<div className="buttons">
 				<div className="mb-1">
 					<button
